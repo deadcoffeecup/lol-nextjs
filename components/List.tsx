83fa9427c393563a8b8b9686ca533b2f234430a3
deadcoffeecup <tsikora555@gmail.com>
2022-09-10 23:47:14 +0200

    champions array only when fetchedData isn't empty

--- components/List.tsx
+++ components/List.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useFetch } from '../hooks/useFetch';
 import { championsAPI } from '../constants/apis';
 
 export const List = () => {
-  const { fetchedObj } = useFetch(championsAPI);
-  console.log(fetchedObj);
+  const { fetchedData } = useFetch(championsAPI);
+  const [championsArr, setChampionsArr] = useState<any>([]);
+  useEffect(() => {
+    if (!!Object.keys(fetchedData).length) {
+      setChampionsArr(Object.values(fetchedData.data));
+    }
+  }, [fetchedData]);
+
+  console.log(championsArr);
+
   return <div>List</div>;
 };
--- hooks/useFetch.ts
+++ hooks/useFetch.ts
@@ -1,16 +1,16 @@
 import { useEffect, useState } from 'react';
 
 export const useFetch = (API: string) => {
-  const [fetchedObj, setFetchedObj] = useState({});
+  const [fetchedData, setFetchedData] = useState<any>({});
   useEffect(() => {
     setTimeout(() => {
       fetch(API)
         .then((res) => res.json())
-        .then((json) => setFetchedObj(json))
+        .then((json) => setFetchedData(json))
         .catch((err) =>
           console.error(`Fetch error with: \n ${API} \n ${err} `)
         );
     }, 100);
   }, []);
-  return { fetchedObj };
+  return { fetchedData };
 };
