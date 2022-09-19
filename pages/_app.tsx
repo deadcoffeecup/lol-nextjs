816449189ad30d4146fd9a8b429b60299a3cfe98
deadcoffeecup <tsikora555@gmail.com>
2022-09-19 17:27:38 +0200

    react-query-devtools added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 export const queryClient = new QueryClient();
 
@@ -7,6 +8,7 @@ function App({ Component, pageProps }) {
   return (
     <QueryClientProvider client={queryClient}>
       <Component {...pageProps} />
+      <ReactQueryDevtools />
     </QueryClientProvider>
   );
 }
