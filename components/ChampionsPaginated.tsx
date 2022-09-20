a73fe4842156f80ec1433e0e6d46f699ad1b03d7
deadcoffeecup <tsikora555@gmail.com>
2022-09-20 14:51:14 +0200

    little routing

--- components/ChampionsPaginated.tsx
+++ components/ChampionsPaginated.tsx
@@ -1,3 +1,4 @@
+import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react';
 import { championAvatarAPI } from '../constants/apis';
 import { getChampions } from '../hooks/getChampions';
@@ -5,7 +6,7 @@ import { ChampionType } from '../types/champion-data.types';
 
 export const ChampionsPaginated = () => {
   const { data, isLoading } = getChampions();
-
+  const router = useRouter();
   const [championsCount, setChampionsCount] = useState<number>(5);
   const [showedData, setShowedData] = useState<ChampionType[]>(
     [] as ChampionType[]
@@ -21,13 +22,16 @@ export const ChampionsPaginated = () => {
     }
   }, [data]);
   return (
-    <div>
+    <div className='champs'>
       {isLoading && <h2>Loading...</h2>}
       {showedData?.map((champion: ChampionType) => (
-        <div key={champion.id}>
+        <div className='champ' key={champion.id}>
           <div>Name: {champion.name}</div>
-          <img src={championAvatarAPI + champion.name + '.png'} />
-          <div>Motto: {champion.blurb}</div>
+          <img
+            onClick={() => router.push(`champion/${champion.id}`)}
+            src={championAvatarAPI + champion.name + '.png'}
+          />
+          <div>Motto: {champion.lore}</div>
           <ul>
             {champion.tags.map((tagName) => (
               <li key={tagName}>{tagName}</li>
@@ -35,6 +39,22 @@ export const ChampionsPaginated = () => {
           </ul>
         </div>
       ))}
+
+      <style jsx>{`
+        .champs {
+          width: 100%;
+          display: flex;
+          flex-wrap: wrap;
+          justify-content: center;
+        }
+        .champ {
+          background-color: #2222;
+          border-radius: 5px;
+          padding: 20px;
+          text-align: center;
+          max-width: 30%;
+        }
+      `}</style>
     </div>
   );
 };
--- components/List.tsx
+++ components/List.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useFetch } from '../hooks/useFetch';
 import { championsAPI, championAvatarAPI } from '../constants/apis';
-import Image from 'next/image';
 import { ChampionType } from '../types/champion-data.types';
 import { useRouter } from 'next/router';
 
